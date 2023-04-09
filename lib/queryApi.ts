13a5a2ab07618868d7b6c410f24172008c30bb94
jianhong kang <kang0230@flinders.edu.au>
2023-04-09 10:18:29 +0930

    modified:   lib/chatgpt.ts 	modified:   lib/queryApi.ts 	modified:   components/ChatRow.tsx 	modified:   components/LoginPage.tsx 	modified:   components/Message.tsx 	modified:   components/NewChat.tsx 	modified:   lib/chatgpt.ts 	modified:   lib/queryApi.ts

--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -9,15 +9,17 @@ const query = async (text: string, chatId: string, model: string) => {
       prompt: text,
       temperature: 0.9,
       top_p: 1,
-      max_tokens: 4000,
+      max_tokens: 5000,
       frequency_penalty: 0.5,
       presence_penalty: 0,
     });
 
     return res.data.choices[0].text;
+
   } catch (error) {
     console.error(`ChatGPT was unable to answer! (Error: ${error})`);
     return undefined;
+
   }
 };
 
